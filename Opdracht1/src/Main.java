70bceb5ff5a4a49625a847c9bed7069cd4fc5ae6
LukaHerrmann <lukajherrmann@gmail.com>
2021-04-25 19:01:23 +0200

    Class gemaakt voor het uitvoeren en testen van de classes.

---
+++ Opdracht1/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] arg) {
+        Adress adr1 = new Adress("Nagtglasstraat 26", "4331 WJ", "Middelburg", "Nederland");
+
+        Buyer b1 = new Buyer("Willem");
+        b1.setDeliveryAdress(adr1);
+
+        Seller s1 = new Seller("Josef");
+        s1.setReturnAdress(adr1);
+        System.out.println(b1);
+        System.out.println(s1);
+    }
+}
